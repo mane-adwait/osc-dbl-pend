3c29f4da54f781acd893968292d2fbc09218b0c5
Adwait Mane <mane.learn.2023.04@gmail.com>
2025-01-29 13:52:06 -0500

    New file.

---
+++ sandbox_1.m
@@ -0,0 +1,14 @@
+
+syms a;
+b = [a;2*a];
+disp(size(a));
+disp(size(b));
+syms t c(t);
+d = [c;c];
+disp(size(c));
+disp(size(d));
+disp(numel(d));
+
+% Notes
+% b is of type sym (symbolic variable), 
+% not symfun (symbolic function)
