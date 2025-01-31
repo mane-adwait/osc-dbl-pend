ed3424c697689ecae67a32c57d5d625a0e90659c
Adwait Mane <mane.learn.2023.04@gmail.com>
2025-01-31 14:28:38 -0500

    New code.

--- sandbox_1.m
+++ sandbox_1.m
@@ -1,16 +1,26 @@
-clear all
-syms a;
-b = [a;2*a];
-disp(size(a));
-disp(size(b));
+clc; clear all
 
-% Notes
-% b is of type sym (symbolic variable), 
-% not symfun (symbolic function)
+fprintf(['Takeaway: size and numel work for symbolic variables (type sym), ' ...
+    '\n not symbolic functions (type symfun) \n\n'])
+
+fprintf(['Use matrix mutiplication for indexing, ' ...
+    'e.g [1 0]*d to index the first value of d, [0 1]*d to index the second'])
+
+syms a;
+b = [a; 2*a];
+disp(['size(a) = ', num2str(size(a))]);
+disp(['numel(a) = ', num2str(numel(a))]);
+disp(['size(b) = ', num2str(size(b))]);
+disp(['numel(b) = ', num2str(numel(b))]);
+fprintf('\n')
 
 syms t c(t);
 d = [a;c];
-disp(size(c));
-disp(size(d));
-disp(numel(d));
+disp(['size(c) = ', num2str(size(c))]);
+disp(['numel(c) = ', num2str(numel(c))]);
+disp(['size(d) = ', num2str(size(d))]);
+disp(['numel(d) = ', num2str(numel(d))]);
 
+% Appendix.
+% disp(['size(??) = ', num2str(size(_))]);
+% disp(['numel(??) = ', num2str(numel(_))]);
