5c9540fa1c8d7d6c8472e93832119bc0f05490db
Adwait Mane <mane.learn.2023.04@gmail.com>
2025-01-31 14:03:38 -0500

    Modify.

--- sandbox_1.m
+++ sandbox_1.m
@@ -3,12 +3,14 @@ syms a;
 b = [a;2*a];
 disp(size(a));
 disp(size(b));
+
+% Notes
+% b is of type sym (symbolic variable), 
+% not symfun (symbolic function)
+
 syms t c(t);
 d = [a;c];
 disp(size(c));
 disp(size(d));
 disp(numel(d));
 
-% Notes
-% b is of type sym (symbolic variable), 
-% not symfun (symbolic function)
