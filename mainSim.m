cb7e74cf7891e8275b0277fedad74239ab4ca803
Adwait Mane <mane.learn.2023.04@gmail.com>
2025-02-04 17:13:06 -0500

    Minor edit.

--- mainSim.m
+++ mainSim.m
@@ -5,6 +5,6 @@ clc; clear; close all;
 
 odefun = @(t,x) dynamics(t,x,[0;0]);
 
-[tout, xout] = ode45(odefun, [0 5], [pi/4; 0; pi/4; 0])
+[tout, xout] = ode45(odefun, [0 5], [pi/4; 0; pi/4; 0]) ;
 
 plot(tout,xout(:,1))
