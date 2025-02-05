f8baf29b2c2b99c708e05fd8f1434c3dedc46674
Adwait Mane <mane.learn.2023.04@gmail.com>
2025-02-05 18:05:03 -0500

    Minor edit.

--- mainSim.m
+++ mainSim.m
@@ -5,6 +5,7 @@ clc; clear; close all;
 
 odefun = @(t,x) dynamics(t,x,[0;0]);
 
-[tout, xout] = ode45(odefun, [0 5], [pi/4; 0; pi/4; 0]) ;
+% State: x = [th1; dth1; th2; dth2]
+[tout, xout] = ode45(odefun, [0 5], [-pi/2; 0; 0.1; 0]) ;
 
-plot(tout,xout(:,1))
+plot(tout,xout(:,1)); xlabel('Time (s)'); ylabel('Angle (rad)');
