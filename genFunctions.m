a855ea33a47a1090f246a26d44cc0a6df392385e
Adwait Mane <mane.learn.2023.04@gmail.com>
2025-02-06 18:12:32 -0500

    Minor edits.

--- genFunctions.m
+++ genFunctions.m
@@ -34,31 +34,31 @@ dy = diff(y,t);
 ddy = diff(dy,t);
 
 % Forward kinematics: center positions of each body.
-r1 = [0.5*p.L1*cos(th1); 0.5*p.L1*sin(th1)]
+r1 = [0.5*p.L1*cos(th1); 0.5*p.L1*sin(th1)];
 r2 = [p.L1*cos(th1) + 0.5*p.L2*cos(th1+th2); 
-    p.L1*sin(th1) + 0.5*p.L2*sin(th1+th2)]
+    p.L1*sin(th1) + 0.5*p.L2*sin(th1+th2)];
 
 % Velocities.
-dr1 = diff(r1,t)
-dr2 = diff(r2,t)
+dr1 = diff(r1,t);
+dr2 = diff(r2,t);
 
 % Accelerations.
-ddr1 = diff(dr1,t)
-ddr2 = diff(dr2,t)
+ddr1 = diff(dr1,t);
+ddr2 = diff(dr2,t);
 
 % Kinetic energy: 0.5*m*v (dot) v
-T = 0.5*p.m1*dr1.'*dr1 + 0.5*p.m2*dr2.'*dr2
+T = 0.5*p.m1*dr1.'*dr1 + 0.5*p.m2*dr2.'*dr2 ;
 % Potential energy: m*g*h
-V = p.m1*p.g*[0 1]*r1 + p.m2*p.g*[0 1]*r2
+V = p.m1*p.g*[0 1]*r1 + p.m2*p.g*[0 1]*r2 ;
 
 % Lagrangian
-L = T- V
+L = T - V ;
 
 
 % Euler-Lagrange equation
-dL_dq = diff(L,q)
-dL_ddq = gradient(L,dq)
-d_dt_dL_ddq = diff(dL_ddq,t)
+dL_dq = diff(L,q) ;
+dL_ddq = gradient(L,dq) ;
+d_dt_dL_ddq = diff(dL_ddq,t) ;
 
 EL_LHS = d_dt_dL_ddq - dL_dq;
 
