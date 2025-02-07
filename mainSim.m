b59be97e005d703811a1266a7a5f5aa276bf87ca
Adwait Mane <mane.learn.2023.04@gmail.com>
2025-02-07 18:02:54 -0500

    Add pseudocode.

--- mainSim.m
+++ mainSim.m
@@ -5,16 +5,21 @@ clc; clear; close all;
 
 odefun = @(t,x) dynamics(t,x,[0;0]);
 
-t_span = [0 5]; % Total duration (s).
-dt = 0.01; % Sampling time (s). The state is sampled and the QP is run once each interval.
+t_span = [0 1]; % Total duration (s).
+dt = 0.1; % Sampling time (s). The state is sampled and the QP is run once each interval.
 % Control rate = 1/dt (Hz).
 t_vector = t_span(1):dt:t_span(2);
+x_vector = zeros(4,numel(t_vector));
+x_vector(:,1) = [-pi/2; 0; 0.1; 0]; % State: x = [th1; dth1; th2; dth2].
 
 % Simulation loop.
 for iter = 1:numel(t_vector)-1
-    sim_t_span = [t_vector(iter) t_vector(iter)+1];
+    sim_t_span = [t_vector(iter) t_vector(iter+1)];
 
-    % State: x = [th1; dth1; th2; dth2]
-    [tout, xout] = ode45(odefun, sim_t_span, [-pi/2; 0; 0.1; 0]) ;
+    [tout, xout] = ode45(odefun, sim_t_span, x_vector(:,iter) ) ;
+    
+    % Interpolate tout and xout, and replace the preallocated x_vector with
+    % the interpolated values.
+end
 
 plot(tout,xout(:,1)); xlabel('Time (s)'); ylabel('Angle (rad)');
