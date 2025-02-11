2bd0be2695e227911839aa80fac9abaee78e75f4
Adwait Mane <mane.learn.2023.04@gmail.com>
2025-02-10 21:10:38 -0500

    Initialize the output vectors.

--- mainSim.m
+++ mainSim.m
@@ -9,15 +9,24 @@ t_span = [0 1]; % Total duration (s).
 dt = 0.1; % Sampling time interval (s). The state is sampled and the QP is run once each interval.
 % Control rate = 1/dt (Hz).
 t_vector = t_span(1):dt:t_span(2);
-x_vector = zeros(4,numel(t_vector));
-x_vector(:,1) = [-pi/2; 0; 0.1; 0]; % State: x = [th1; dth1; th2; dth2].
+
+% Initialize the output vectors. We will append the simulation outputs to
+% these vectors.
+t_out = t_span(1); 
+% State: x = [th1; dth1; th2; dth2].
+x_out = [-pi/2; 0; 0.1; 0]; % Initial state.
+% x_vector = zeros(4,numel(t_vector));
+% x_vector(:,1) = [-pi/2; 0; 0.1; 0]; % State: x = [th1; dth1; th2; dth2].
+
 
 % Simulation loop.
 for iter = 1:numel(t_vector)-1
     sim_t_span = [t_vector(iter) t_vector(iter+1)];
 
-    [tout, xout] = ode45(odefun, sim_t_span, x_vector(:,iter) ) ;
+    [t_iter_out, x_iter_out] = ode45(odefun, sim_t_span, x_vector(:,iter) ) ;
     
+    
+
     % Interpolate tout and xout, and replace the preallocated x_vector with
     % the interpolated values.
     % vq = interp1(x,v,xq)
