b7124c99bd2557f6e24377aad67b77b4b2e0f58b
Adwait Mane <mane.learn.2023.04@gmail.com>
2025-02-11 15:53:53 -0500

    New code.

--- mainSim.m
+++ mainSim.m
@@ -6,7 +6,9 @@ clc; clear; close all;
 odefun = @(t,x) dynamics(t,x,[0;0]);
 
 t_span = [0 5]; % Total duration (s).
-dt = 0.01; % Sampling time (s). The state is sampled and the QP is run once each interval.
+% At the start of each interval, the state is sampled, the QP is run, and 
+% the control torque is computed.
+dt = 0.01; % Sampling time interval (s). 
 % Control rate = 1/dt (Hz).
 t_vector = t_span(1):dt:t_span(2);
 
@@ -14,11 +16,15 @@ t_vector = t_span(1):dt:t_span(2);
 x0 = [-pi/2; 0; 0.1; 0]; % Initial state.
 x0_current = x0;
 
+% Store the time and state output.
+t_store = []; x_store = [];
+
 % Simulation loop.
 for iter = 1:numel(t_vector)-1
     sim_t_span = [t_vector(iter) t_vector(iter+1)];
 
     [tout, xout] = ode45(odefun, sim_t_span, x0_current) ;
+    x0_current = xout(end,:).'; % I think this should be the last column. 
 end
 
 plot(tout,xout(:,1)); xlabel('Time (s)'); ylabel('Angle (rad)');
