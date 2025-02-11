929aeca8490ad1bac77d84896d3e64e5010eabfd
Adwait Mane <mane.learn.2023.04@gmail.com>
2025-02-10 21:18:52 -0500

    Partial bug fix.

--- mainSim.m
+++ mainSim.m
@@ -5,7 +5,7 @@ clc; clear; close all;
 
 odefun = @(t,x) dynamics(t,x,[0;0]);
 
-t_span = [0 1]; % Total duration (s).
+t_span = [0 0.2]; % Total duration (s).
 dt = 0.1; % Sampling time interval (s). The state is sampled and the QP is run once each interval.
 % Control rate = 1/dt (Hz).
 t_vector = t_span(1):dt:t_span(2);
@@ -22,10 +22,10 @@ x_out = [-pi/2; 0; 0.1; 0]; % Initial state.
 for iter = 1:numel(t_vector)-1
     sim_t_span = [t_vector(iter) t_vector(iter+1)];
 
-    [t_iter_out, x_iter_out] = ode45(odefun, sim_t_span, x_vector(:,iter) ) ;
+    [t_iter_out, x_iter_out] = ode45(odefun, sim_t_span, x_out(:,end) ) ;
     
-    t_out = [t_out t_iter_out];
-    x_out = [x_out x_iter_out];
+    t_out = [t_out t_iter_out(2:end)];
+    x_out = [x_out x_iter_out(:,2:end)];
 
     % Interpolate tout and xout, and replace the preallocated x_vector with
     % the interpolated values.
