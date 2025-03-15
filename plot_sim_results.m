3b28a35ed5c883c2d934c49203cbce3696dc42af
Adwait Mane <mane.learn.2023.04@gmail.com>
2025-03-14 21:44:00 -0400

    New code.

--- plot_sim_results.m
+++ plot_sim_results.m
@@ -94,4 +94,4 @@ figure(10); cFig = gcf; movegui(cFig,'northwest');
 % Set the axes properties
 cAx = gca;
 lineColors = linspecer(numPlots);
-axis on; 
+axis on; cAx.TickLabelInterpreter = 'latex';
